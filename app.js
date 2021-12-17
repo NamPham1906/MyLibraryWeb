5133899fab3e3509393990902e1029ece45a8c41
NamPham1906 <61858329+NamPham1906@users.noreply.github.com>
2021-12-17 18:39:17 +0700

    add stockkeeper menu

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var loginRouter = require('./routes/loginRoutes/index');
 var chairmanRouter = require('./routes/chairmanRoutes/index');
+var stockkeeperRouter = require('./routes/stockkeeperRoutes/index');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', loginRouter);
 app.use('/chairman', chairmanRouter);
+app.use('/stockkeeper', stockkeeperRouter);
 
 
 // catch 404 and forward to error handler
