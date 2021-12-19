c11771e1586f9aa330a6d9ee93853c1c3aec5292
dieuhuong1211 <huong.bn.vt@gmail.com>
2021-12-19 21:31:19 +0700

    phiếu mượn

---
+++ routes/librarianRoutes/borrow_form/borrowFormService.js
@@ -0,0 +1,35 @@
+const { raw } = require('express');
+const { NULL } = require('node-sass');
+const {models} = require('../../../models/index');
+
+const { Op } = require("sequelize");
+
+exports.list=(page = 0, itemPerPage = 10 ) =>{
+    return models.phieumuonsach.findAll({
+        offset:page*itemPerPage,
+        limit: itemPerPage,
+        raw:true
+    });
+};
+
+exports.total=()=>{
+    return models.phieumuonsach.count();
+}
+
+exports.getUser=(userId)=>{
+    return models.docgia.findOne({
+        where:{
+            madocgia: userId
+        },
+        raw: true
+    })
+}
+
+
+exports.totalBooks=(borrowId)=>{
+    return models.sachthuocphieumuon.count({
+        where:{
+            maphieumuon: borrowId
+        }
+    });
+}
