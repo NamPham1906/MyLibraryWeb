a1fd3c981fac60be6e4d743aa7297b6ae815132f
khoale <kthedestroy@gmail.com>
2021-12-19 15:42:35 +0700

    login

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var chairmanRouter = require('./routes/chairmanRoutes/index');
 var stockkeeperRouter = require('./routes/stockkeeperRoutes/index');
 var librarianRouter = require('./routes/librarianRoutes/index');
 var cashierRouter = require('./routes/cashierRoutes/index');
+const bodyParser = require('body-parser');
 const passport = require('./routes/loginRoutes/passport');
 var flash = require('connect-flash');
 
@@ -23,8 +24,9 @@ app.set('view engine', 'hbs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:process.env.SESSION_SECRET}));
+app.use(session({secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(function (req, res, next) {
@@ -41,10 +43,57 @@ app.use(function(req, res, next){
 		res.redirect('/');
 	}
 })
-app.use('/chairman', chairmanRouter);
-app.use('/stockkeeper', stockkeeperRouter);
-app.use('/librarian', librarianRouter);
-app.use('/cashier', cashierRouter);
+app.use('/chairman', function(req, res, next){
+	if(req.user){
+		const bophan = req.user.bophan;
+		if(bophan==="Ban Giám Đốc"){
+			next();
+		}else{
+			res.redirect('/');
+		}
+	}else{
+		res.redirect('/');
+	}
+}, chairmanRouter);
+
+app.use('/stockkeeper',function(req, res, next){
+	if(req.user){
+		const bophan = req.user.bophan;
+		if(bophan==="Thủ Kho"){
+			next();
+		}else{
+			res.redirect('/');
+		}
+	}else{
+		res.redirect('/');
+	}
+}, stockkeeperRouter);
+
+app.use('/librarian', function(req, res, next){
+	if(req.user){
+		const bophan = req.user.bophan;
+		if(bophan==="Thủ Thư"){
+			next();
+		}else{
+			res.redirect('/');
+		}
+	}else{
+		res.redirect('/');
+	}
+},librarianRouter);
+
+app.use('/cashier',function(req, res, next){
+	if(req.user){
+		const bophan = req.user.bophan;
+		if(bophan==="Thủ Quỹ"){
+			next();
+		}else{
+			res.redirect('/');
+		}
+	}else{
+		res.redirect('/');
+	}
+}, cashierRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
